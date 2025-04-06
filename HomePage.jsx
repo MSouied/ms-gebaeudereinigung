8b8e03323afab926fce11945bb8ba626f7478ca8
ms-gebaeudereinigung <xfire80@hotmail.com>
2025-04-07 00:29:41 +0200

    Aktualisieren von HomePage.jsx

--- HomePage.jsx
+++ HomePage.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 function HomePage() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <img
+        src="/images/logo-ms-gebaeudereinigung.png"
+        alt="MS Gebäudereinigung Logo"
+        style={{ width: '180px', marginBottom: '20px' }}
+      />
       <h1>Willkommen bei unserem Reinigungsservice!</h1>
       <p>Dies ist eine Beispielseite für unsere zukünftige Website.</p>
     </div>
