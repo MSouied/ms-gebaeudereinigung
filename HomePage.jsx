f9aa489dabc4c6e69b3b7ffffe17c2c476db0127
ms-gebaeudereinigung <xfire80@hotmail.com>
2025-04-06 18:17:56 +0200

    Aktualisieren von HomePage.jsx

--- HomePage.jsx
+++ HomePage.jsx
@@ -1,1 +1,12 @@
-delete HomePage.jsx
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div style={{ fontFamily: 'Arial', padding: '2rem' }}>
+      <h1>Willkommen bei MS Gebäudereinigung</h1>
+      <p>Professionelle Reinigungsdienste für Wohnungen, Büros und mehr.</p>
+    </div>
+  );
+};
+
+export default HomePage;
