2b76f1f4e05397ad1fd626f8a7d336851ceba3f1
ms-gebaeudereinigung Lohr am Main <xfire80@hotmail.com>
2025-04-07 16:21:47 +0200

    Create HomePage.jsx
    Create HomePage.jsx

---
+++ HomePage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      {/* شعار الشركة */}
+      <img
+        src="/logo-ms-gebaeudereinigung.PNG"
+        alt="MS Gebäudereinigung Logo"
+        style={{ width: '200px', marginBottom: '20px' }}
+      />
+
+      {/* منشور الخدمات */}
+      <img
+        src="/flyer-ms-gebaeudereinigung.PNG"
+        alt="Flyer MS Gebäudereinigung"
+        style={{ width: '300px', marginBottom: '20px' }}
+      />
+
+      {/* العنوان */}
+      <h1>Willkommen bei unserem Reinigungsservice!</h1>
+
+      {/* وصف */}
+      <p>Dies ist eine Beispielseite für unsere zukünftige Website.</p>
+    </div>
+  );
+}
+
+export default HomePage;
